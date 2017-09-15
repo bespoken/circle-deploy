f6ab6f30e5ee33cac3d35cc63313400f08a943bc
John Kelvie <john.kelvie@gmail.com>
2017-09-14 19:55:29 -0500

    Added files

---
+++ deployment.sh
@@ -0,0 +1,32 @@
+echo "Image: $1"
+echo "Env: $2"
+echo "Branch/Tag: $3"
+echo "FIP: $4"
+echo "Size: $5"
+
+export SERVICE=$1-$2
+echo "Service: $SERVICE"
+
+# Write out environment variables
+env > env.out
+
+# install hyper
+wget https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz
+tar xzf hyper-linux-x86_64.tar.gz
+chmod +x hyper
+./hyper --help
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker build -f docker/Dockerfile -t bespoken/$1:$3 .
+docker push bespoken/$1:$3
+./hyper config --accesskey $HYPER_KEY --secretkey $HYPER_SECRET
+./hyper login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+./hyper pull bespoken/$1:$3
+./hyper rm -f $SERVICE || true
+./hyper run -d \
+	--env-file env.out \
+    --name $SERVICE \
+    --size $5 \
+    -P bespoken/$1:$3
+./hyper fip attach -f $4 $SERVICE
+	  
