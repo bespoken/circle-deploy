c59665a4a11ace40cbad77ad4b1ec8c412840f2b
John Kelvie <jpk@bespoken.io>
2018-12-26 13:40:24 -0500

    Cleanup of update and create when services are inactive

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const FargateHelper = require("./lib/FargateHelper");
 
 FargateHelper.run().then(() => {
 	console.log("DONE");
+}).catch((e) => {
+	console.error(e);
+	process.exit(1);
 });
 
 process.on("unhandledRejection", (e) => {
